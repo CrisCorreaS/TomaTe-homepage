e8f252cd21b4e0e35ec1f8efbe99f769f7c30083
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-01 02:40:56 +0100

    project colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-green": "#445946",
+        "secondary-green": "#CAD9C4",
+        "primary-brown-light": "#D5BDA1",
+        "primary-brown-dark": "#864622",
+        "secondary-brown": "#C78853",
+        "bg-light": "#F7FFF4",
+        "bg-dark": "#383330"
+      },
+    },
   },
   plugins: [],
 };
