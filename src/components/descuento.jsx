9d4f5ecf2af4f62cb18415a8a6879a326020e6d0
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-05 08:00:43 +0100

    descuento ok

---
+++ src/components/descuento.jsx
@@ -0,0 +1,14 @@
+function Descuento() {
+  return (
+    <>
+      <article class="fixed top-0 left-0 w-full bg-bg-dark text-bg-light font-baskerville py-1 px-4 shadow-lg flex items-center justify-center">
+        <p class="text-center">
+          10% de descuento en tu primera compra con{" "}
+          <strong class="font-semibold text-sm">TomaTé</strong>
+        </p>
+      </article>
+    </>
+  );
+}
+
+export default Descuento;
