eab6c113bce4270dae3aaa169e970fedd190d45e
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-06 03:49:57 +0100

    descuento updated

--- src/components/descuento.jsx
+++ src/components/descuento.jsx
@@ -1,10 +1,10 @@
 function Descuento() {
   return (
     <>
-      <article class="fixed top-0 left-0 w-full bg-bg-dark text-bg-light font-baskerville py-1 px-4 shadow-lg flex items-center justify-center">
-        <p class="text-center">
+      <article className="fixed top-0 left-0 w-full bg-bg-dark text-bg-light font-baskerville py-1 px-4 shadow-lg flex items-center justify-center text-sm">
+        <p className="text-center">
           10% de descuento en tu primera compra con{" "}
-          <strong class="font-semibold text-sm">TomaTé</strong>
+          <strong className="font-semibold text-sm">TomaTé</strong>
         </p>
       </article>
     </>
