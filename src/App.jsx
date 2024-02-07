782b6086c7039749823a51dd5efaf5c509527303
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-07 05:56:23 +0100

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Descuento from "./components/descuento";
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <header className="mt-6">
+        <Descuento></Descuento>
+        <NavBar></NavBar>
+      </header>
+      <main></main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </>
+  );
 }
