c619535ffb28b645b1cd013ef4f0f0b60391ce32
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-08 11:11:32 +0100

    quoteTitle updated

--- src/components/quoteTitle.jsx
+++ src/components/quoteTitle.jsx
@@ -7,14 +7,14 @@ const QuoteTitle = ({
 }) => {
   return (
     <article
-      className={`w-full ${colorBg} ${colorTxt} font-baskerville py-1 px-4 shadow-lg text-center text-xs sm:text-xs`}
+      className={`w-full ${colorBg} ${colorTxt} font-baskerville py-5 px-4 shadow-lg text-center text-xs sm:text-xs`}
     >
       <h5 className="my-4 text-xl tracking-tight font-bold opacity-85 2xl:mt-6 2xl:mb-5 2xl:text-4xl md:mt-4 md:mb-3 md:text-3xl md:leading-tight">
         {titulo}
       </h5>
       <p className="text-sm mb-4 italic md:text-lg md:mb-6">{texto}</p>
       {children && ( // Usamos una expresión condicional para renderizar el botón solo si se pasa como prop
-        <div className="flex justify-center my-4 lg:my-6">{children}</div>
+        <div className="flex justify-center text-sm my-4 lg:my-6">{children}</div>
       )}
     </article>
   );
