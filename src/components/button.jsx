ba725008d0fca16216d84cd7885f0bd708b8a69e
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-08 05:59:42 +0100

    button update

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,15 @@
-function Button({ children = "Leer Más", enlace = "http://127.0.0.1:5173/" }) {
+function Button({
+  colorLetras = "bg-bg-light",
+  fondo = "text-bg-dark",
+  animacion = "hover:bg-gray-100",
+  children = "Leer Más",
+  enlace = "http://127.0.0.1:5173/",
+}) {
   return (
     <a href={enlace} target="_blank" rel="noreferrer">
       <button
         type="button"
-        className="font-baskerville uppercase py-2 px-7 inline-flex items-center font-semibold rounded-full border border-transparent bg-bg-light text-bg-dark hover:bg-gray-100 cursor-pointer"
+        className={`${colorLetras} ${fondo} font-baskerville uppercase py-2 px-7 inline-flex items-center font-semibold rounded-full border border-transparent ${animacion} cursor-pointer`}
       >
         {children}
       </button>
