3b59caae9da05c7caaab5f6427169f6c96c538ec
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-08 06:00:57 +0100

    quoteTitle updated

--- src/components/quoteTitle.jsx
+++ src/components/quoteTitle.jsx
@@ -8,9 +8,9 @@ const QuoteTitle = ({
     <article
       className={`w-full ${colorBg} ${colorTxt} font-baskerville py-1 px-4 shadow-lg text-center text-xs sm:text-xs`}
     >
-      <h3 className="my-4 text-xl tracking-tight font-bold opacity-85 2xl:mt-6 2xl:mb-5 2xl:text-4xl md:mt-4 md:mb-3 md:text-3xl md:leading-tight">
+      <h5 className="my-4 text-xl tracking-tight font-bold opacity-85 2xl:mt-6 2xl:mb-5 2xl:text-4xl md:mt-4 md:mb-3 md:text-3xl md:leading-tight">
         {titulo}
-      </h3>
+      </h5>
       <p className="text-sm mb-4 italic md:text-lg md:mb-6">{texto}</p>
     </article>
   );
