cb4ce0fda7f103752f5f5a9fa82b0b51e0a2fc34
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-09 05:36:03 +0100

    blogPost added

---
+++ src/components/blogPost.jsx
@@ -0,0 +1,30 @@
+import imagen6 from "../assets/img/img6.jpg"
+import imagen7 from "../assets/img/img7.png"
+import imagen8 from "../assets/img/img8.jpg"
+
+const BlogPost = ({ children, colorFondo = "bg-primary-green", imageNumber, titulo = "Té & Horóscopo", texto="Tu horóscopo revela tus gustos más refinados. Acompáñanos en un viaje de sabor y estrellas en busca de tu té ideal"}) => {
+  let imagenes=[imagen6, imagen7, imagen8]
+
+  return (
+    <article className={`${colorFondo} text-bg-light m-auto overflow-hidden shadow-lg cursor-pointer transition duration-100 ease-in-out transform hover:shadow-2xl h-90 w-70 md:w-90`}>
+      <a href="#" className="block w-full h-full">
+        <img
+          alt="blog photo"
+          src= {imagenes[imageNumber]}
+          className="object-cover w-full max-h"
+        />
+        <main className="w-full py-6 px-16 font-alegreya flex flex-col items-center text-center">
+          <p className="mb-2 text-xl uppercase font-bold">
+            {titulo}
+          </p>
+          <p className="font-baskerville font-light py-5">
+            {texto}
+          </p>
+        </main>
+        <footer className="pb-10 w-full flex justify-center">{children}</footer>
+      </a>
+    </article>
+  );
+};
+
+export default BlogPost;
